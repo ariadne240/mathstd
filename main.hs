9354e659db6e5e1bee093c0228d4637c6496fbaf
Ariadne <zeuskimone@gmail.com>
2018-11-19 14:24:01 +0900

    HOE

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 -- main
 import System.IO
 import System.Process
+-- import Control.Concurrent (forkIO)
 import Welcome
 import Fancyput
 
@@ -34,12 +35,19 @@ loginfail = do
 ismem :: String -> String -> IO Bool
 ismem x y = do
  let input = "idpwin daybreak/username" ++ " " ++ x ++ " " ++ y
- out <- readProcess "daybreak/purebreak" [] input
--- (Just hin, Just hout, _, _) <- createProcess (proc "daybreak/purebreak" []){ std_in = CreatePipe, std_out = CreatePipe }
--- hPutStr hin input
--- out <- hGetContents hout
- --let out2 = (head . tail . tail . lines) out
- --print out2
+-- out <- readProcess "daybreak/purebreak" [] input
+ (Just hin, Just hout, _, ph) <- createProcess (proc "daybreak/purebreak" []){ std_in = CreatePipe, std_out = CreatePipe }
+{--
+It is an issue that the 'cwd' in createProcess works environment-dependently...
+If you get a problem then try this instead:
+let input = "idpwin daybreak/username" ++ " " ++ x ++ " " ++ y
+
+--}
+ hPutStr hin input
+ hClose hin
+ out <- hGetLine hout
+ hClose hout
+-- waitForProcess ph
  case (out) of
   "True" -> return True
   "True\n" -> return True
